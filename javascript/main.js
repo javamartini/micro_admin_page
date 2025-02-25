6275ef9b28ed3217164c0357ad1e399bca436a69
javamartini <javamartini@proton.me>
2025-02-24 23:19:07 -0600

    Added PWA functionality, including icons

--- javascript/main.js
+++ javascript/main.js
@@ -13,3 +13,9 @@ document.querySelector("#btnPrecipMeasure").addEventListener("click", function()
     setUserPrecipSettings()
     gatherData() //reloads data with user's selection
 })
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/service_worker.js")
+      .then(() => console.log("Service Worker Registered"))
+      .catch((error) => console.log("Service Worker Registration Failed", error));
+}  
